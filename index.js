0edfd6e9af68dd8e73a5a6626f71b69e3dec87df
Gaganjot-Singh7 <jot77430@gmail.com>
2024-05-30 20:22:01 +0530

    deploy api

---
+++ index.js
@@ -0,0 +1,25 @@
+const express=require("express")
+const DbConnection=require("./database/Connection.js")
+const server=express()
+const router=require("./router/index.js")
+const dotenv=require("dotenv");
+require("./database/productModel.js")
+
+dotenv.config();
+server.get("/",(req,res)=>{
+    res.send("hi, im live")
+})
+const port=process.env.port || 8080
+server.use("/api/products",router)
+const start=async()=>{
+try {
+    server.listen(8080,()=>{
+        console.log(`server connected http://localhost:${port}/api/products`);
+    })
+} catch (error) {
+    console.log("server start",error);
+}
+
+}
+start();
+DbConnection();
